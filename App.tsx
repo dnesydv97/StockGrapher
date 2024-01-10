e418ff6e83bc589d6f4e7cebb62d2ea7c9d1b5ae
Dinesh yadav <dnes4353@gmail.com>
2024-01-10 16:32:27 +0545

    makes the chart dynamic

--- App.tsx
+++ App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import {Theme} from './src/utils';
 import Routes from './src/routes';
-import {Provider} from 'react-redux';
-import {store} from './src/store';
+
 import {StatusBar} from 'react-native';
 import { colors } from './src/utils/theme';
 
@@ -11,12 +10,13 @@ const theme = Theme.createTheme();
 
 const App = () => {
   return (
-    <Provider store={store}>
+   <>
       <StatusBar barStyle="dark-content" backgroundColor={colors.greyWhite} />
       <ThemeProvider theme={theme}>
         <Routes />
       </ThemeProvider>
-    </Provider>
+      </>
+  
   );
 };
 
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,85 +1,104 @@
-
-
 import React, {useCallback, useEffect, useState} from 'react';
 import {Dropdown, Chart, Container, SpacerColumn, Row} from '../../components';
-import { fetchStockData } from '../../apis/services';
+import {fetchStockData} from '../../apis/services';
 
 const Home = () => {
-
   const [symbol, setSymbol] = useState('IBM');
-  const [selectedChartItem, setSelectedChartItem] = useState("");
+  const [selectedChartItem, setSelectedChartItem] = useState('');
   const [stockData, setStockData] = useState(null);
-  console.log('symbol', symbol)
-  console.log('selectedChartItem', selectedChartItem)
-  console.log('stockData new', stockData)
-  
-  
+  const [stockFilter, setStockFilter] = useState([]);
+  console.log('symbol', symbol);
+  console.log('selectedChartItem', stockFilter);
+  // console.log('stockData new', stockData)
+
+   // Fetch stock data on company name  change
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await fetchStockData(symbol);
         setStockData(data);
       } catch (error) {
-        console.log("error",error)
+        console.log('error', error);
       }
     };
 
     fetchData();
   }, [symbol]);
-  
-  const handleSymbolChange = useCallback((newSymbol) => {
+
+  // Update stock filter when selected chart item changes
+  useEffect(() => {
+    if (stockData) {
+      const valuesWithDates = [];
+
+      for (const date in stockData['Weekly Adjusted Time Series']) {
+        const value =
+          stockData['Weekly Adjusted Time Series'][date][selectedChartItem];
+
+        valuesWithDates.push({date, value});
+      }
+      setStockFilter(valuesWithDates)
+      console.log('@@@@@@@@@@@@@@@@@@@@@@@@', valuesWithDates);
+
+      console.log('@@@@@@@@@@@@@#############', selectedChartItem);
+    }
+  }, [selectedChartItem]);
+
+// Update chart data when stock filter changes
+  const apiResponseData = [
+    {date: '2024-01-09', value: '160.0800'},
+    {date: '2024-01-05', value: '159.1600'},
+    {date: '2023-12-29', value: '163.5500'},
+    {date: '2023-12-22', value: '162.1400'},
+    {date: '2023-12-15', value: '162.2300'},
+    {date: '2023-12-08', value: '161.9600'},
+    {date: '2023-12-01', value: '160.5500'},
+    {date: '2023-11-24', value: '155.1800'},
+    {date: '2023-11-17', value: '152.8900'},
+    {date: '2023-11-17', value: '152.8900'},
+    {date: '2023-11-17', value: '152.8900'},
+    {date: '2023-11-17', value: '152.8900'},
+ 
+  ];
+  const [data, setData] = useState({
+    labels: apiResponseData?.map(entry => entry.date),
+    datasets: [
+      {
+        data: apiResponseData?.map(entry => parseFloat(entry.value)),
+      },
+    ],
+  });
+  //Event handlers
+  const handleSymbolChange = useCallback(newSymbol => {
     setSymbol(newSymbol);
   }, []);
 
-  const handleChartChange = useCallback((newSelectedItem) => {
+  const handleChartChange = useCallback(newSelectedItem => {
     setSelectedChartItem(newSelectedItem);
   }, []);
 
+   // Dropdown options
   const symbolOptions = [
-    { label: "IBM", value: "IBM" },
-    { label: "MSFT", value: "MSFT" },
+    {label: 'IBM', value: 'IBM'},
+    {label: 'MSFT', value: 'MSFT'},
   ];
 
   const chartOptions = [
-    { label: "Open", value: "1. open" },
-    { label: "High", value: "2. high" },
-    { label: "Low", value: "3. low" },
-    { label: "Close", value: "4. close" },
+    {label: 'Open', value: '1. open'},
+    {label: 'High', value: '2. high'},
+    {label: 'Low', value: '3. low'},
+    {label: 'Close', value: '4. close'},
   ];
 
-  const [data, setData] = useState({
-        labels: [
-          '2020/01/4',
-          '2040/10/23',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec',
-        ],
-        datasets: [
-          {
-            data: [72, 810, 700, 723, 493, 677, 641, 509, 213, 335, 198, 249],
-          },
-        ],
-      });
+
  
-
-
-
  
 
   return (
     <Container>
       <Row alignItems="center" justifyContent="space-between">
-      <Dropdown
+        <Dropdown
           items={symbolOptions}
-          containerStyle={{ width: "45%" }}
+          containerStyle={{width: '45%'}}
           defaultValue={symbol}
           onChange={handleSymbolChange}
           placeholder="Select a symbol"
@@ -87,7 +106,7 @@ const Home = () => {
         />
         <Dropdown
           items={chartOptions}
-          containerStyle={{ width: "45%" }}
+          containerStyle={{width: '45%'}}
           defaultValue={selectedChartItem}
           onChange={handleChartChange}
           placeholder="Select an item"
@@ -97,20 +116,11 @@ const Home = () => {
 
       <SpacerColumn size={5} />
       <Chart data={data} width={undefined} height={390} />
-     
     </Container>
   );
 };
 
 export default Home;
-
-
-
-
-
-
-
-
 
 // import React, {useCallback, useEffect, useState} from 'react';
 // import {Dropdown, Chart, Container, SpacerColumn, Row} from '../../components';
@@ -148,8 +158,6 @@ export default Home;
 //     { label: "Close", value: "4. close" },
 //   ];
 
-
-
 //   const [details, setDetails]= useState()
 //   const [data, setData] = useState({
 //     labels: [
@@ -174,52 +182,46 @@ export default Home;
 //   });
 
 //   useEffect(() => {
-  
+
 //     let data = ' Display the list of repositories on the mobile app below the search bar.\r\n● Implement an elegant UI/UX design that is suitable for a mobile environment.\r\n● Display additional information about each repository, such as the number of stars, forks,\r\nand the primary language.\r\n';
-    
+
 //     let config = {
 //       method: 'get',
 //       maxBodyLength: Infinity,
 //       url: 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=MSFT&apikey=EGE9ODG4XG0ATFV8',
-//       headers: { 
+//       headers: {
 //         'Content-Type': 'application/json'
 //       },
 //       data : data
 //     };
-    
+
 //     axios.request(config)
 //     .then((response) => {
 //       setDetails(response.data)
- 
- 
+
 //     })
 //     .catch((error) => {
 //       console.log(error);
 //     });
-    
-//   }, []); 
 
-
- 
-
+//   }, []);
 
 // // useEffect(()=>{
 // //   if(details){
 // //   const valuesWithDates = [];
 // //   const valuesWithAmount = [];
-  
+
 // //   for (const date in details["Weekly Adjusted Time Series"]) {
 // //     const value = details["Weekly Adjusted Time Series"][date][selectedItem];
 // //     const amount = details["Weekly Adjusted Time Series"][selectedItem];
 // //     valuesWithDates.push({ date, value});
 // //     valuesWithAmount.push({  amount});
-  
+
 // // }
 // // console.log("@@@@@@@@@@@@@@@@@@@@@@@@",valuesWithDates)
 // // console.log("valuesWithAmount",valuesWithAmount)
 // // console.log("@@@@@@@@@@@@@#############",selectedItem)}
 // // },[selectedItem])
- 
 
 //   return (
 //     <Container>
